50e1327df146c76ee4b79b7e0b453f17cfa0fa06
parc314 <66965818+parc314@users.noreply.github.com>
2022-07-27 18:35:00 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,113 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Client } from "pg";
+const nodeCron = require("node-cron");
+const axios = require('axios');
+const moment = require('moment');
+const { decode } = require('url-encode-decode');
+
+
+const app = express();
+dotenv.config(); //Reads .env file and makes it accessible via process.env
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const createTableQuery = `
+CREATE TABLE videos (
+  id  SERIAL PRIMARY KEY,  
+  title varchar,
+  description varchar,
+  publishedAt varchar,
+  thumbnailUrl varchar
+);
+`;
+
+const connectToDB = async () => {
+  try {
+    await client.connect();
+    await client.query(createTableQuery);
+    console.log('Table is successfully created');
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
+
+
+
+app.get("/get/videos", async (req: Request, res: Response, next: NextFunction) => {
+    const pageNumber = req.query.pageNumber || 1;
+    const pageSize = req.query.pageSize || 10;
+    const getVideosQuery = `
+    SELECT *
+    FROM videos
+    ORDER BY publishedat DESC
+    OFFSET ${pageNumber}
+    LIMIT ${pageSize}
+    `;
+    try{
+        const response = await client.query(getVideosQuery);
+        res.send({
+            data: response.rows,
+            pageNumber,
+            pageSize
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+app.get("/get/video", async (req: Request, res: Response, next: NextFunction) => {
+  const getVideoWithParam = `
+  SELECT *
+  FROM videos WHERE title='${decode(req.query.title)}' AND description='${decode(req.query.description)}'
+  `;
+  try{
+        const response = await client.query(getVideoWithParam);
+        res.send(response.rows);
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+
+const searchVideos = async () => {
+  try {
+    let res = await axios.get(`https://www.googleapis.com/youtube/v3/search?order=date&type=video&key=AIzaSyDDMG7LP9TGLYzqLwFeF7_fWk1jAyUeV9o&publishedAfter=${moment().format("YYYY-MM-DD[T]HH:mm[:00Z]")}&q=cricket&part=snippet`)
+    let publishedAt = '', title = '', description = '', thumbnails = '';
+
+    for (let item of res.data.items) {
+      publishedAt = item.snippet.publishedAt;
+      title = item.snippet.title;
+      description = item.snippet.description;
+      thumbnails = item.snippet.thumbnails.default.url;
+
+      const insertVideoQuery = `
+      INSERT INTO videos (title, description, publishedAt, thumbnailUrl)
+      VALUES ('${title}', '${description}', '${publishedAt}', '${thumbnails}')
+      `;
+      console.log(insertVideoQuery)
+      try {
+        await client.query(insertVideoQuery);
+      } catch (err) {
+        console.log(err);
+        continue;
+      }
+      console.log('Data insert successful');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+const job = nodeCron.schedule("*/10 * * * * *", searchVideos);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${process.env.PORT}`);
+});
 
